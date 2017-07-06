a39b1304f90f7fb1e152d2ffb6f87cdb742dcd55
Colton Seal <coltonsealtt@gmail.com>
2017-07-06 15:58:16 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const Locations = require('./lib/Locations');
+const Departments = require('./lib/Departments');
+const Events = require('./lib/Events');
+const Roles = require('./lib/Roles');
+const Shifts = require('./lib/Shifts');
+const TimePunches = require('./lib/TimePunches');
+const Users = require('./lib/Users');
+
+module.exports = {
+  Locations: Locations,
+  Departments: Departments,
+  Events: Events,
+  Roles: Roles,
+  Shifts: Shifts,
+  TimePunches: TimePunches,
+  Users: Users
+}
